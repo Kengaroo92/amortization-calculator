41122e8e7aa79f35db84ab649ecb28bd9d01592e
Adrian Edelen <adrianedelen@gmail.com>
2020-10-16 14:55:09 -0400

    math is functional working with numbers
    using accounting.js the math is functional and accurate, no more floating point math issues.
    
    currently the amortization schedule prints out to the console, everything is ready to be plugged in to a table

--- amortization-calculator/scripts/main.js
+++ amortization-calculator/scripts/main.js
@@ -1,5 +1,4 @@
-﻿import { Decimal } from '/scripts/modules/decimal.mjs';
-
+﻿
 document.getElementById("calculate").addEventListener("click", () => {
 	calculate();
 })
@@ -9,31 +8,32 @@ function calculate() {
 	//get amount/total principle from DOM
 	//let amount = parseInt(document.getElementById("amount").value);
 	//get Interest from DOM, change to decimal
-	//let rate = parseInt(document.getElementById("rate").value) / 100;
+	//let rate = parseInt(document.getElementById("rate").value);
 	// get loan term from DOM in months
 	//let term = parseInt(document.getElementById("Months").value);
 	let amount = 5000;
-	let rate = .02;
+	let rate = 2;
 	let term = 12;
-	let DAmount = new Decimal(amount);
-	let DRate = new Decimal(rate);
-	let DTerm = new Decimal(term);
-	// calculates monthly payment
-	let payment = (DAmount) * (dRate / 1200) / (1 - (1 + DRate / 1200) ** (DTerm - (DTerm * 2)));
+	amount = accounting.toFixed(amount, 2);
+	rate = accounting.toFixed(rate, 2);
+	term = accounting.toFixed(term, 2);
 
+	// calculates monthly payment
+	let payment = (amount) * (rate / 1200) / (1 - (1 + rate / 1200) ** (0 - term));
+	payment = accounting.toFixed(payment, 2);
 	//declare vars for the loop
 	let remainingBal = amount;
 	let interestPayment;
 	let principalPayment;
 	let month;
-	let totalInterest = 0;
+	let totalInterest = 00;
 	for (let i = 0; i < term; i++) {
 		//in the amortization table, the following things should be printed. e
 		month = i + 1;
-		interestPayment = remianingBal * rate / 1200;
-		totalInterest += interestPayment;
-		principalPayment = payment - interestPayment;
-		remainingBal = remainingBal - principalPayment;
+		interestPayment = accounting.toFixed(remainingBal * rate / 1200, 2);
+		totalInterest = totalInterest + parseFloat(interestPayment);
+		principalPayment = accounting.toFixed(payment - interestPayment, 2);
+		remainingBal = accounting.toFixed(remainingBal - principalPayment, 2);
 
 		//document.getElementById("MonthTable").innerHTML = month;
 		//document.getElementById("paymentTable").innerHTML = payment;
@@ -41,13 +41,13 @@ function calculate() {
 		//document.getElementById("interestTable").innerHTML = interestPayment;
 		//document.getElementById("totalIntTable").innerHTML = totalInterest;
 		//document.getElementById("BalanceTable").innerHTML = remainingBal;
-		console.log(month);
-		console.log(payment);
-		console.log(principalPayment);
-		console.log(interestPayment);
-		console.log(interestPayment);
-		console.log(totalInterest);
-		console.log(remainingBal);
+		console.log("month: " + month);
+		console.log("payment: " + payment);
+		console.log("principal payment: " + principalPayment);
+		console.log("interest payment: " + interestPayment);
+		console.log("total interest: " + totalInterest);
+		console.log("remaining balance: " + remainingBal);
+		console.log("");
 	}
 
 
@@ -55,22 +55,22 @@ function calculate() {
 //verify user input
 //any element that has class .numberOnly will be limited to
 //only numbers being input
-document.querySelectorAll(".numbersOnly").forEach(a => {
-	a.addEventListener("keydown", (evt) => {
-		let keyChar = (evt.which) ? evt.which : evt.keyCode
+//document.querySelectorAll(".numbersOnly").forEach(a => {
+//	a.addEventListener("keydown", (evt) => {
+//		let keyChar = (evt.which) ? evt.which : evt.keyCode
 
-		if (keyChar >= 48 && keyChar <= 57 ||
-			keyChar >= 96 && keyChar <= 105 ||
-			keyChar == 08 ||
-			keyChar == 37 ||
-			keyChar == 39) {
-			return true;
-		} else {
-			evt.preventDefault();
-			return false;
-		}
-	})
-})
+//		if (keyChar >= 48 && keyChar <= 57 ||
+//			keyChar >= 96 && keyChar <= 105 ||
+//			keyChar == 08 ||
+//			keyChar == 37 ||
+//			keyChar == 39) {
+//			return true;
+//		} else {
+//			evt.preventDefault();
+//			return false;
+//		}
+//	})
+//})
 
 
 
