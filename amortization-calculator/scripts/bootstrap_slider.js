bfa67d3c4cc3ce219bd609bf7864bf14f23ee7d3
Fred <valhallatechnc@gmail.com>
2020-10-18 22:06:52 -0400

    Next to last update. This is a big one.
    Added FontAwesome files and reference pointers to render the GitHub image on the navbar
    Changed the color scheme
    Added some classes and IDs to the main CSS
    Minor code refactoring

--- amortization-calculator/scripts/bootstrap_slider.js
+++ amortization-calculator/scripts/bootstrap_slider.js
@@ -1,6 +1,21 @@
-
-let sldrLoanTerm = new Slider('#sldrLoanTerm', {
+//Renders range slider
+"use strict";
+let sldrLoanTerm = new Slider("#sldrLoanTerm", {
 	formatter: function(value) {
-		return 'Current value: ' + value;
+		return "Current value: " + value;
 	}
+});
+
+$("btnMnth").click(function ()
+{
+    $("p").hide("slow", function ()
+    {
+        alert("The paragraph is now hidden");
+    });
+});$("btnYear").click(function ()
+{
+    $("p").hide("slow", function ()
+    {
+        alert("The paragraph is now hidden");
+    });
 });
